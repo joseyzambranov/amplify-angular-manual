ef127efcfef5455d1cad994a15484ad88f8f095c
jose zambrano <jose.zambrano@sinapsiscorp.com>
2023-08-02 18:10:38 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,27 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  import { Amplify, Auth } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: 'arn:aws:cognito-idp:us-east-1:912844262341:userpool/us-east-1_r7okF5k9o',
+
+    // REQUIRED - Amazon Cognito Region
+    region: 'us-east-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'us-east-1_r7okF5k9o',
+
+
+
+
+  },
+});
+
+
+// You can get the current config object
+const currentConfig = Auth.configure();
